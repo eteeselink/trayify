133fb5288d6f854c85bbd1a98372528f026e7ad3
Egbert Teeselink <e@teeselink.nl>
2012-11-27 19:42:29 +0100

    nesrtd

--- trayify/main.c
+++ trayify/main.c
@@ -14,7 +14,7 @@ static BOOL CALLBACK FilterWindow( HWND hwnd, LPARAM lParam )
 
     GetWindowThreadProcessId(hwnd, &windowPID);
     if (windowPID == args->processId) 
-	{
+    {
         sbpush(args->handles, hwnd);
     }
 
@@ -24,9 +24,9 @@ static BOOL CALLBACK FilterWindow( HWND hwnd, LPARAM lParam )
 HWND* GetWindowsForProcess(int processId)
 {
     FilteredWindows result;
-	
-	result.processId = processId;
-	result.handles = NULL;
+    
+    result.processId = processId;
+    result.handles = NULL;
 
     if (EnumWindows(&FilterWindow, (LPARAM) &result ) == FALSE ) {
       return NULL;
@@ -37,72 +37,111 @@ HWND* GetWindowsForProcess(int processId)
 /** Waits for the process to spawn at least one window, and returns the handle to one such window. */
 HWND WaitForWindow(HANDLE process)
 {
-	DWORD processId;
-	HWND* handles;
-	int len = 0;
+    DWORD processId;
+    HWND* handles;
+    int len = 0;
 
-	processId = GetProcessId(process);
-	printf("\n pid: %d", processId);
+    processId = GetProcessId(process);
+    printf("\n pid: %d", processId);
 
-	while(len == 0)
-	{
-		handles = GetWindowsForProcess(processId);
-		len = sbcount(handles);
-		Sleep(200);
-	}
+    while(len == 0)
+    {
+        handles = GetWindowsForProcess(processId);
+        len = sbcount(handles);
+        Sleep(200);
+    }
 
-	return handles[0];
+    return handles[0];
 }
 
 #define CMDLINE_BUF_SIZE 200
 HANDLE ExecuteCmdLine(int argc, char** argv)
 {
-	BOOL ok;
-	char cmdLine[CMDLINE_BUF_SIZE];
-	int i;
-	SHELLEXECUTEINFO shellExecuteInfo;
+    BOOL ok;
+    char cmdLine[CMDLINE_BUF_SIZE];
+    int i;
+    SHELLEXECUTEINFO shellExecuteInfo;
 
-	memset(&shellExecuteInfo, 0, sizeof(shellExecuteInfo));
-	shellExecuteInfo.cbSize = sizeof(shellExecuteInfo);
+    memset(&shellExecuteInfo, 0, sizeof(shellExecuteInfo));
+    shellExecuteInfo.cbSize = sizeof(shellExecuteInfo);
 
-	cmdLine[0] = '\0';
-	for(i = 2; i < argc; i++)
-	{
-		strcat_s(cmdLine, CMDLINE_BUF_SIZE, argv[i]);
-		strcat_s(cmdLine, CMDLINE_BUF_SIZE, " ");
-	}
+    cmdLine[0] = '\0';
+    for(i = 2; i < argc; i++)
+    {
+        strcat_s(cmdLine, CMDLINE_BUF_SIZE, argv[i]);
+        strcat_s(cmdLine, CMDLINE_BUF_SIZE, " ");
+    }
 
-	shellExecuteInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
-	shellExecuteInfo.lpVerb = "open";
-	shellExecuteInfo.lpFile = argv[1];
-	shellExecuteInfo.lpParameters = cmdLine;
-	shellExecuteInfo.nShow = SW_SHOWDEFAULT;
-	ok = ShellExecuteEx(&shellExecuteInfo);
-	if(!ok)
-	{
-		printf("Error: %d", GetLastError());
-		exit(-1);
-	}
+    shellExecuteInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+    shellExecuteInfo.lpVerb = "open";
+    shellExecuteInfo.lpFile = argv[1];
+    shellExecuteInfo.lpParameters = cmdLine;
+    shellExecuteInfo.nShow = SW_SHOWDEFAULT;
+    ok = ShellExecuteEx(&shellExecuteInfo);
+    if(!ok)
+    {
+        printf("Error: %d", GetLastError());
+        exit(-1);
+    }
 
-	return shellExecuteInfo.hProcess;
+    return shellExecuteInfo.hProcess;
 }
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message) 
+    {
+            //case WM_COMMAND:
+              // handle menu selections etc.
+            //break;
+            //case WM_PAINT:
+              // draw our window - note: you must paint something here or not trap it!
+            //break;
+            case WM_DESTROY:
+                 PostQuitMessage(0);
+            break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
+
+HWND CreateMainWindow()
+{
+    static const char* class_name = "TRAYIFY_MESSAGE_WND";
+    WNDCLASSEX wx;
+    HWND mainWnd = 0;
+
+    memset(&wx, 0, sizeof(wx));
+    wx.cbSize = sizeof(WNDCLASSEX);
+    wx.lpfnWndProc = WndProc;        // function which will handle messages
+    wx.hInstance = (HINSTANCE)GetModuleHandle(NULL);
+    wx.lpszClassName = class_name;
+
+    if(RegisterClassEx(&wx)) 
+    {
+        mainWnd = CreateWindowEx( 0, class_name, "Trayify Message Window", 0, 0, 0, 0, 0, HWND_MESSAGE, NULL, NULL, NULL );
+    }
+
+    return mainWnd;
+}
 
 int main(int argc, char** argv)
 {
-	HANDLE process;
-	HWND mainWindow;
+    HANDLE process;
+    HWND targetWnd;
 
-	process = ExecuteCmdLine(argc, argv);
-	mainWindow = WaitForWindow(process);
-	
-	{
-		char buf[100];
-		GetWindowText(mainWindow, buf, 100);
-		//ShowWindow(handles[i], SW_HIDE);
-		printf("\n  Found: %s", buf);
-	}
+    process = ExecuteCmdLine(argc, argv);
+    targetWnd = WaitForWindow(process);
+    
+    {
+        char buf[100];
+        GetWindowText(targetWnd, buf, 100);
+        //ShowWindow(handles[i], SW_HIDE);
+        printf("\n  Found: %s", buf);
+    }
 
 
-	return 0;
+    return 0;
 }
